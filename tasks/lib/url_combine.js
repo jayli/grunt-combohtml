6daa0922a7e35377190bf163195026aae35add3b
弘树 <tiehang.lth@alibaba-inc.com>
2014-11-21 16:06:10 +0800

    bugfix for url_combine

--- tasks/lib/url_combine.js
+++ tasks/lib/url_combine.js
@@ -82,16 +82,18 @@ function getCombinedAssets(arr) {
 
 	var toReplacedPrefix = 'http://' + baseUrl + '/';
 	arr.forEach(function(jsPath, idx){
-		if(jsPath.indexOf('??') != -1) {
-			// 如果该 js 引用中已经包含 combo，需要先解开 combo
-			var subComboParsed = jsPath.split('??');
-			var prefix = subComboParsed[0].replace(toReplacedPrefix, '');
-			var subComboAssets = subComboParsed[1].split(',');
-			subComboAssets.forEach(function(subComboAsset){
-				source.push(path.join(prefix, subComboAsset));
-			});
-		} else {
-			source.push(jsPath.replace(toReplacedPrefix, ''));
+		if (jsPath.indexOf(baseUrl) != -1) {
+			if (jsPath.indexOf('??') != -1) {
+				// 如果该 js 引用中已经包含 combo，需要先解开 combo
+				var subComboParsed = jsPath.split('??');
+				var prefix = subComboParsed[0].replace(toReplacedPrefix, '');
+				var subComboAssets = subComboParsed[1].split(',');
+				subComboAssets.forEach(function (subComboAsset) {
+					source.push(path.join(prefix, subComboAsset));
+				});
+			} else {
+				source.push(jsPath.replace(toReplacedPrefix, ''));
+			}
 		}
 	});
 	return (source.length > 0) ? ('http://' + baseUrl + '/??' + source.join(',')).replace('????', '??') : '';
